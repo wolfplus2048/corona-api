9e150ba1e40d5c2caf4c2769865f099b7b5fecdd
wolfplus2048 <wolfplus@gmail.com>
2020-04-12 21:53:49 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/wolfplus2048/corona-api
+
+go 1.13
+
+require github.com/spf13/viper v1.6.3
---
+++ main.go
@@ -0,0 +1,47 @@
+package corona
+import (
+	"context"
+	"github.com/spf13/viper"
+	"net"
+)
+type Session interface {
+	Push(route string, v interface{}) error
+	UID() string
+	Bind(ctx context.Context, uid string)
+	Kick(ctx context.Context) error
+	OnClose(c func()) error
+	Close()
+	RemoteAddr() net.Addr
+	Set(key string, value interface{}) error
+	Get(key string) interface{}
+}
+
+type App interface {
+	AddAcceptor(addr string, certs ...string) error
+	RegisterHandler()
+	RegisterModule()
+	Configure(isFrontend bool, serverType string, serverMetadata map[string]string, cfgs ...*viper.Viper) error
+	Start()
+}
+
+// Module is the interface that represent a module.
+type Module interface {
+	Init() error
+	AfterInit()
+	BeforeShutdown()
+	Shutdown() error
+}
+
+// BindingStorage interface
+type BindingStorage interface {
+	GetUserFrontendID(uid, frontendType string) (string, error)
+	PutBinding(uid string) error
+}
+
+// Component is the interface that represent a component.
+type Component interface {
+	Init()
+	AfterInit()
+	BeforeShutdown()
+	Shutdown()
+}
