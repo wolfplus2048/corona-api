2158cb337218bdedb144820bd5df44250d9ed350
wolfplus2048 <wolfplus@gmail.com>
2020-04-14 10:46:00 +0800

    add

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/wolfplus2048/corona-api
 
-go 1.13
+go 1.14
 
 require github.com/spf13/viper v1.6.3
--- main.go
+++ main.go
@@ -1,48 +1,24 @@
 package corona
+
 import (
-	"context"
-	"github.com/spf13/viper"
-	"net"
+	"plugin"
 )
-type Session interface {
-	Push(route string, v interface{}) error
-	UID() string
-	Bind(ctx context.Context, uid string)
-	Kick(ctx context.Context) error
-	OnClose(c func()) error
-	Close()
-	RemoteAddr() net.Addr
-	Set(key string, value interface{}) error
-	Get(key string) interface{}
-}
 
-type App interface {
-	AddAcceptor(addr string, certs ...string) error
-	Register(c Component, name string)
-	RegisterRemote(c Component, name string)
-	RegisterModule(module Module, name string) error
-	Configure(isFrontend bool, serverType string, serverMetadata map[string]string, cfgs ...*viper.Viper) error
-	Start()
-}
-
-// Module is the interface that represent a module.
-type Module interface {
-	Init() error
-	AfterInit()
-	BeforeShutdown()
-	Shutdown() error
-}
-
-// BindingStorage interface
-type BindingStorage interface {
-	GetUserFrontendID(uid, frontendType string) (string, error)
-	PutBinding(uid string) error
-}
-
-// Component is the interface that represent a component.
-type Component interface {
-	Init()
-	AfterInit()
-	BeforeShutdown()
-	Shutdown()
+var (
+	app App
+)
+func init()  {
+	p, err := plugin.Open("./corona.so")
+	if err != nil {
+		panic(err)
+	}
+	sym, err := p.Lookup("CoronaApp")
+	if err != nil {
+		panic(err)
+	}
+	a, ok := sym.(App)
+	if !ok {
+		panic("expecting corona.app interface")
+	}
+	app = a
 }
